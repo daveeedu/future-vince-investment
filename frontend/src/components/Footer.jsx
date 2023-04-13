671034a367c7b2666327bb78fd007c8208ec9244
David Edu <daveeedu@gmail.com>
2023-04-13 16:19:38 +0100

    fix responsivenes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import { FaInstagramSquare, FaGem, FaLinkedinIn, FaCopyright, FaHouseUser, FaEnvelope, FaPhone } from 'react-icons/fa';
-import { SiFacebook, SiTwitter } from 'react-icons/si';
-import SimpleMap from './SimpleMap';
-
+import {
+  FaInstagramSquare,
+  FaGem,
+  FaLinkedinIn,
+  FaCopyright,
+  FaHouseUser,
+  FaEnvelope,
+  FaPhone,
+} from "react-icons/fa";
+import { SiFacebook, SiTwitter } from "react-icons/si";
+import SimpleMap from "./SimpleMap";
 
 const Footer = () => {
   return (
     <div className=" backgrnd-0 ">
       <div className="row bg m-auto  pt-5 ">
         <div className="col-md-4 display-6 mb-5 img-foot logo">
-        <h5 className="text-start text-white text-uppercase ms-5 mb-4 mt-5 flex"><FaGem className='mr-3 mt-1'/> <span>Certificate</span></h5>
+          <h5 className="text-start text-white text-uppercase ms-5 mb-4 mt-5 flex">
+            <FaGem className="mr-3 mt-1" /> <span>Certificate</span>
+          </h5>
           <img className="img-footTwo" src="./images/cert-invest.jpg" alt="#" />
         </div>
         <div className="col-md-2 text-white mb-4 mt-5">
@@ -32,39 +41,41 @@ const Footer = () => {
           </ul>
         </div>
         <div className="col-md-3 mb-4 mt-5">
-        <h5 className="text-start text-uppercase text-white">Contact</h5>
+          <h5 className="text-start text-uppercase text-white">Contact</h5>
           <ul className="list-unstyled text-start">
             <li className="text-white flex mb-2">
-              <FaHouseUser className="mt-1 mr-3" />  <span>1 Tesla Road Austin, TX 78725</span>
+              <FaHouseUser className="mt-1 mr-3" />{" "}
+              <span>1 Tesla Road Austin, TX 78725</span>
             </li>
             <li className="text-white flex mb-2">
-              <FaEnvelope className="mt-1 mr-3"/>  <span>elontrade.info@gmail.com</span>
+              <FaEnvelope className="mt-1 mr-3" />{" "}
+              <span>elontrade.info@gmail.com</span>
             </li>
             <li className="text-primary flex">
-              <FaPhone className="mt-1 mr-3"/>  <span>+1 (972) 920-6191</span>
+              <FaPhone className="mt-1 mr-3" /> <span>+1 (972) 920-6191</span>
             </li>
           </ul>
         </div>
       </div>
       <div className="flex justify-between bg m-auto rights bgBlack text-white mt-5 px-5 py-3">
-        <div className='flex'>
-        <FaCopyright className='mt-1 mr-2'/> 
-        <span >2022</span>
+        <div className="flex">
+          <FaCopyright className="mt-1 mr-2" />
+          <span>2022</span>
         </div>
-      
-          <p className=""> Elon Trade. All rights reserved</p>
+
+        <p className=""> Elon Trade. All rights reserved</p>
         <div className="flex justify-between  ">
-          <a href="https://facebook.com"> 
-          <SiFacebook className="me-2 text-white" /> 
+          <a href="https://instagram.com/elon_trader?igshid=YmMyMTA2M2Y=" target="_blank">
+            <SiFacebook className="me-2 text-white" />
           </a>
-          <a href="https://instagram.com">
+          <a href="https://instagram.com/elon_trader?igshid=YmMyMTA2M2Y=" target="_blank">
             <FaInstagramSquare className="me-2 text-white" />
           </a>
-          <a href="https://twitter.com">
+          <a href="https://instagram.com/elon_trader?igshid=YmMyMTA2M2Y=" target="_blank">
             <SiTwitter className="me-2 text-white" />
           </a>
-          <a href="https://linkedin.com">
-          <FaLinkedinIn className="text-white" />
+          <a href="https://instagram.com/elon_trader?igshid=YmMyMTA2M2Y=" target="_blank">
+            <FaLinkedinIn className="text-white" />
           </a>
         </div>
       </div>
@@ -72,8 +83,4 @@ const Footer = () => {
   );
 };
 
-
 export default Footer;
-
-
-   
